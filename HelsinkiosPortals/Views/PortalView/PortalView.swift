25a5c6007f5de780c56204eda5fd436579db9075
Matti Dahlbom <mdahlbom666@gmail.com>
2024-03-14 16:25:58 +0200

    Add occlusion portal in the form of an alleyway

--- HelsinkiosPortals/Views/PortalView/PortalView.swift
+++ HelsinkiosPortals/Views/PortalView/PortalView.swift
@@ -46,27 +46,34 @@ struct PortalView: View {
         RealityView { content in
             let doorYPosition: Float = 1.04
 
-            let landscapePortal = LandscapeDoorPortal()
-            landscapePortal.orientation *= simd_quatf(angle: Constants.deg35, axis: [0, 1, 0])
-            landscapePortal.position = [-1.7, doorYPosition, -1.5]
-            landscapePortal.enableGroundShadow()
-            content.add(landscapePortal)
-            
-            let corridorPortal = CorridorDoorPortal()
-            corridorPortal.position = [-0.2, doorYPosition, -2.2]
-            corridorPortal.enableGroundShadow()
-            content.add(corridorPortal)
-
-            let videoPortal = VideoDoorPortal()
-            videoPortal.orientation *= simd_quatf(angle: -Constants.deg50, axis: [0, 1, 0])
-            videoPortal.position = [1.4, doorYPosition, -1.6]
-            videoPortal.enableGroundShadow()
-            content.add(videoPortal)
-
-            let logo = createLogo()
-            logo.position = [0.3, 2.5, -1.8]
-            logo.enableGroundShadow()
-            content.add(logo)
+//            let landscapePortal = LandscapeDoorPortal()
+//            landscapePortal.orientation *= simd_quatf(angle: Constants.deg35, axis: [0, 1, 0])
+//            landscapePortal.position = [-1.7, doorYPosition, -1.5]
+//            landscapePortal.enableGroundShadow()
+//            content.add(landscapePortal)
+//            
+//            let corridorPortal = CorridorDoorPortal()
+//            corridorPortal.position = [-0.2, doorYPosition, -2.2]
+//            corridorPortal.enableGroundShadow()
+//            content.add(corridorPortal)
+//
+//            let videoPortal = VideoDoorPortal()
+//            videoPortal.orientation *= simd_quatf(angle: -Constants.deg50, axis: [0, 1, 0])
+//            videoPortal.position = [1.4, doorYPosition, -1.6]
+//            videoPortal.enableGroundShadow()
+//            content.add(videoPortal)
+//
+//            let logo = createLogo()
+//            logo.position = [0.3, 2.5, -1.8]
+//            logo.enableGroundShadow()
+//            content.add(logo)
+//            
+//            let windowPortal = createWindowPortal()
+//            windowPortal.position = [0.0, 1.2, -1]
+//            content.add(windowPortal)
+            let alleyPortal = createAlleyPortal()
+            alleyPortal.position = [0, 0, -1]
+            content.add(alleyPortal)
         } update: { content in
             if let landscapeDoorPortal = content.entities.first(where: { $0 is LandscapeDoorPortal }) as? DoorPortal {
                 landscapeDoorPortal.setState(state: landscapePortalState)
@@ -126,6 +133,37 @@ struct PortalView: View {
         return logo
     }
     
+    private func createAlleyPortal() -> Entity {
+        let alleyPortal = try! Entity.load(named: "AlleyPortal", in: realityKitContentBundle)
+        
+        // Override the placeholder material on Occlusion_Cube with OcclusionMaterial
+        // to hide geometry on the other side of the portal.
+        let occlusionEntity = alleyPortal.findEntity(named: "Occlusion_Cube") as! ModelEntity
+        let occlusionMaterial = OcclusionMaterial()
+        occlusionEntity.model!.materials = [occlusionMaterial]
+
+        return alleyPortal
+    }
+
+    /// Creates an ImageBasedLightComponent from a single-color source image. This can be
+    /// used to override default scene lighting.
+//    private func createIBLComponent() -> ImageBasedLightComponent {
+//        // Create a CGImage filled with color
+//        let size = CGSize(width: 200, height: 100)
+//        UIGraphicsBeginImageContextWithOptions(size, true, 0.0)
+//        let context = UIGraphicsGetCurrentContext()!
+//        context.setFillColor(UIColor.white.cgColor)
+//        context.fill(CGRect(origin: .zero, size: size))
+//        let cgImage = context.makeImage()!
+//        UIGraphicsEndImageContext()
+//        
+//        // Create the IBL component out of the image
+//        let resource = try! EnvironmentResource.generate(fromEquirectangular: cgImage)
+//        let iblComponent = ImageBasedLightComponent(source: .single(resource), intensityExponent: 10.0)
+//        
+//        return iblComponent
+//    }
+
     /// Returns the DoorPortal entity that is the ancestor of the given entity, or nil if not found.
     private func findDoorPortalAncestor(for entity: Entity) -> DoorPortal? {
         guard let parent = entity.parent else {
